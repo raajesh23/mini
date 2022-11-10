f83ff7ff45d5b268ae21d2236a352f88e6159d38
raajesh23 <62094385+raajesh23@users.noreply.github.com>
2022-11-10 17:17:46 +0530

    Add files via upload

---
+++ mini/mini/Front.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini
+{
+    class Front
+    {
+        public void showFirstscreen()
+        {
+            Console.WriteLine("Welcome to SMS(Student Mgmt. System) v1.0");
+            Console.WriteLine("reveil yourself : \n1. Student \n2. Admin");
+            Console.WriteLine("Enter your choice ( 1 or 2 ) : ");
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    showStudentScreen();
+                    break;
+                case 2:
+                    showAdminScreen();
+                    break;
+            }
+    }
+}
---
+++ mini/mini/student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini
+{
+    class Student
+    {
+           public int id;
+           public char name;
+        public DateTime dob;
+        public void display()
+        {
+            Console.WriteLine("Name of the student is: " +name);
+            Console.WriteLine("Register no of the Student: " + id);
+            Console.WriteLine("Date of birth of student: " + dob);
+        }
+
+        public class enroll
+        {
+            private char student;
+            private char course;
+            private DateTime enrolldt;
+        }
+    }
+}
